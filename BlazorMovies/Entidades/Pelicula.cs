119faec13ba0dbb265ebe708145daad8f5f3178f
Daniel <barreraquezadadanielalejandro@gmail.com>
2026-03-10 20:58:01 -0600

    Primer commit utilizando tecnologia Blazor para practica y proyecto

---
+++ BlazorMovies/Entidades/Pelicula.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorMovies.Entidades;
+
+public class Pelicula
+{
+    public int Id { get; set; } 
+    public required string Titulo { get; set; }
+    public DateTime FechaLanzamiento { get; set; }
+}
